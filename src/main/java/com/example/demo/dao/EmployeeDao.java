70d7eea20f300b80377258f3878a8147eed116a7
Jokerblazes <zcd123@qq.com>
2018-04-25 20:59:00 +0800

    完成根据姓名、性别、年龄条件的查询

--- src/main/java/com/example/demo/dao/EmployeeDao.java
+++ src/main/java/com/example/demo/dao/EmployeeDao.java
@@ -2,6 +2,10 @@ package com.example.demo.dao;
 
 import com.example.demo.po.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 /**
  * @Author Joker
@@ -9,4 +13,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @Date Create in 下午7:34 2018/4/25
  */
 public interface EmployeeDao extends JpaRepository<Employee,Long> {
+
+    @Query(value = "select e from Employee e where e.name=:name")
+    List<Employee> findByName(@Param("name") String name);
+
+    @Query(value = "select e from Employee e where e.age=:age")
+    List<Employee> findByAge(@Param("age") int age);
+
+    @Query(value = "select e from Employee e where e.gender=:gender")
+    List<Employee> findByGender(@Param("gender") int gender);
 }
--- src/test/java/com/example/demo/EmployeeDaoTest.java
+++ src/test/java/com/example/demo/EmployeeDaoTest.java
@@ -8,7 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @Author Joker
@@ -50,8 +54,89 @@ public class EmployeeDaoTest {
         assertEquals(employee1.getName(),employee2.getName());
     }
 
+    @Test
+    public void testDeleteEmployee() {
+        employeeDao.deleteById((long)1);
+        Employee employee = new Employee();
+        employee.setId((long)2);
+        employee.setAge(19);
+        employee.setGender(2);
+        employee.setName("小红");
+        employeeDao.delete(employee);
+    }
 
+    @Test
+    public void testGetAllEmployee() {
+        //小明
+        Employee employee1 = new Employee();
+        employee1.setId((long)1);
+        employee1.setAge(20);
+        employee1.setGender(1);
+        employee1.setName("小明");
 
+        //小红
+        Employee employee2 = new Employee();
+        employee2.setId((long)2);
+        employee2.setAge(19);
+        employee2.setGender(2);
+        employee2.setName("小红");
+        List<Employee> all = employeeDao.findAll();
+        assertTrue(all.size() >= 2);
+        compareEmployeesEqual(all.get(0),employee1);
+        compareEmployeesEqual(all.get(1),employee2);
+    }
 
+    @Test
+    public void testGetEmployeeById() {
+        Employee employee = employeeDao.findById((long) 1).get();
+        //小明
+        Employee employee1 = new Employee();
+        employee1.setId((long)1);
+        employee1.setAge(20);
+        employee1.setGender(1);
+        employee1.setName("小明");
+        compareEmployeesEqual(employee1,employee);
+    }
 
+    @Test
+    public void testGetEmployeeByName() {
+        List<Employee> employees = employeeDao.findByName("小明");
+        assertTrue(employees.size() >= 1);
+
+        //小明
+        Employee employee1 = new Employee();
+        employee1.setId((long)1);
+        employee1.setAge(20);
+        employee1.setGender(1);
+        employee1.setName("小明");
+        compareEmployeesEqual(employee1,employees.get(0));
+    }
+
+    @Test
+    public void testGetEmployeeByAge() {
+        List<Employee> employees = employeeDao.findByAge(20);
+        assertTrue(employees.size() >= 1);
+
+        //小明
+        Employee employee1 = new Employee();
+        employee1.setId((long)1);
+        employee1.setAge(20);
+        employee1.setGender(1);
+        employee1.setName("小明");
+        compareEmployeesEqual(employee1,employees.get(0));
+    }
+
+    @Test
+    public void testGetEmployeeByGender() {
+        List<Employee> employees = employeeDao.findByGender(1);
+        assertTrue(employees.size() >= 1);
+
+        //小明
+        Employee employee1 = new Employee();
+        employee1.setId((long)1);
+        employee1.setAge(20);
+        employee1.setGender(1);
+        employee1.setName("小明");
+        compareEmployeesEqual(employee1,employees.get(0));
+    }
 }
