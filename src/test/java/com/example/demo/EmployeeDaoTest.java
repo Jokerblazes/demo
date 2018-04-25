e3974dc8e5e86dc8221bd388418ce80260fc5692
Jokerblazes <zcd123@qq.com>
2018-04-25 20:30:56 +0800

    任务二：完成Dao层的修改Employee的测试

--- src/test/java/com/example/demo/EmployeeDaoTest.java
+++ src/test/java/com/example/demo/EmployeeDaoTest.java
@@ -28,9 +28,28 @@ public class EmployeeDaoTest {
         employee.setAge(20);
         employee.setGender(1);
         employee.setName("小明");
-        Employee save1 = employeeDao.save(employee);
-        assertEquals(save1,employee);
+        Employee save = employeeDao.save(employee);
+        compareEmployeesEqual(employee,save);
     }
+
+    @Test
+    public void testUpdateEmployee() {
+        Employee employee = new Employee();
+        employee.setId((long)2);
+        employee.setAge(19);
+        employee.setGender(2);
+        employee.setName("小红");
+        Employee save = employeeDao.save(employee);
+        compareEmployeesEqual(employee,save);
+    }
+
+    private void compareEmployeesEqual(Employee employee1,Employee employee2) {
+        assertEquals(employee1.getId(),employee2.getId());
+        assertEquals(employee1.getAge(),employee2.getAge());
+        assertEquals(employee1.getGender(),employee2.getGender());
+        assertEquals(employee1.getName(),employee2.getName());
+    }
+
 
 
 
